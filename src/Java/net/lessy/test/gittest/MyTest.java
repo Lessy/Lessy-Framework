dbbf6bbf81478a87ae32061b9f9e7daf45e3b037
Lessy <lessy@web.de>
2012-04-18 21:27:16 +0200

    java class

---
+++ src/Java/net/lessy/test/gittest/MyTest.java
@@ -0,0 +1,12 @@
+package net.lessy.test.gittest;
+
+/**
+ * @author Hans Lesmeister
+ */
+public class MyTest {
+
+   public static void main(String[] args) {
+      System.out.println("Hi");
+   }
+
+}
