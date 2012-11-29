731d051d66cbe78c940a52e1573ad9b97a9b6d8c
Lessy <lessy@web.de>
2012-11-29 18:55:32 +0100

    1st release on central

---
+++ Common/createBundle.cmd
@@ -0,0 +1,3 @@
+cd target
+jar -cvf bundle.jar lessy-common-1.0.pom lessy-common-1.0.pom.asc lessy-common-1.0.jar lessy-common-1.0.jar.asc lessy-common-1.0-javadoc.jar lessy-common-1.0-javadoc.jar.asc lessy-common-1.0-sources.jar lessy-common-1.0-sources.jar.asc
+pause
